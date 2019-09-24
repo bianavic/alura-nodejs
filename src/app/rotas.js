b158b2d22b9c7e2fbf6ef61246d9cf668ad7dc13
bianavic <bianavic@gmail.com>
2019-09-24 17:48:20 -0300

    trabalhando com middleware method-override para a edição do livro com o método PUT

--- src/app/rotas.js
+++ src/app/rotas.js
@@ -29,34 +29,57 @@ module.exports = (app) => {
             .catch(erro => console.log(erro));
     });
 
-    app.get('/livros/form', function (req, resp) {
-        resp.marko(require('../app/views/livros/form/form.marko'))
-    })
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../app/views/livros/form/form.marko'), { livro: {} });
+    });    
 
-    app.get('/livros/form/:id', function (req, resp) {
+    app.get('/livros/form/:id', function(req, resp) {
         const id = req.params.id;
         const livroDao = new LivroDao(db);
-
+    
         livroDao.buscaPorId(id)
-            .then(livro =>
+            .then(livro => 
                 resp.marko(
-                    require('../views/livros/form/form.marko'), {
-                        livro: livro
-                    }
+                    require('../app/views/livros/form/form.marko'),
+                    { livro: livro }
                 )
             )
             .catch(erro => console.log(erro));
+    
     });
+/*  // opção 1 para adicionar e editar
+    app.post('/livros', function (req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+
+        if (req.body.id != '') {
+            // edicao de livro
+        } else {
+
+        livroDao.adiciona(req.body)
+        .then(resp.redirect('/livros'))
+        .catch(erro => console.log(erro));
+        }
+    }); */
 
     app.post('/livros', function (req, resp) {
         console.log(req.body);
         const livroDao = new LivroDao(db);
 
         livroDao.adiciona(req.body)
-            .then(resp.redirect('/livros'))
-            .catch(erro => console.log(erro));
+        .then(resp.redirect('/livros'))
+        .catch(erro => console.log(erro));
     });
 
+    app.put('/livros', function (req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+
+        livroDao.atualiza(req.body)
+        .then(resp.redirect('/livros'))
+        .catch(erro => console.log(erro));
+    });
+    
     app.delete('/livros/:id', function (req, resp) {
         const id = req.params.id;
         const livroDao = new LivroDao(db);
